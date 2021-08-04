3f75c5801fa63689fc780046ad27400339f57786
janaite <janaite@gmail.com>
2021-08-04 15:51:14 -0300

    more database seed and some test fixes

---
+++ src/main/java/dev/janaite/movieflix/dto/ReviewDTO.java
@@ -0,0 +1,55 @@
+package dev.janaite.movieflix.dto;
+
+import java.io.Serializable;
+
+public class ReviewDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
+	private String text;
+	private Long movieId;
+	private UserDTO user;
+	
+	public ReviewDTO() {
+	}
+	
+	public ReviewDTO(Long id, String text, Long movieId, UserDTO user) {
+		super();
+		this.id = id;
+		this.text = text;
+		this.movieId = movieId;
+		this.user = user;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public UserDTO getUser() {
+		return user;
+	}
+
+	public void setUser(UserDTO user) {
+		this.user = user;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public Long getMovieId() {
+		return movieId;
+	}
+
+	public void setMovieId(Long movieId) {
+		this.movieId = movieId;
+	}
+}
