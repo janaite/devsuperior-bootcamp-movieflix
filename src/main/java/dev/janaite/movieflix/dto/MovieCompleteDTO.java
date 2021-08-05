a586312014f1bb8a33e258c8660a84a39ca99a87
janaite <janaite@gmail.com>
2021-08-05 15:14:16 -0300

    refactor MovieCompleteDTO

--- src/main/java/dev/janaite/movieflix/dto/MovieCompleteDTO.java
+++ src/main/java/dev/janaite/movieflix/dto/MovieCompleteDTO.java
@@ -4,16 +4,10 @@ import java.io.Serializable;
 
 import dev.janaite.movieflix.entities.Movie;
 
-public class MovieCompleteDTO implements Serializable {
+public class MovieCompleteDTO extends MovieSimpleDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
-	private Long id;
-	private String title;
-	private String subTitle;
-	private Integer year;
-	private String imgUrl;
 	private String synopsis;
-
 	private GenreDTO genre;
 
 	public MovieCompleteDTO() {
@@ -21,64 +15,15 @@ public class MovieCompleteDTO implements Serializable {
 
 	public MovieCompleteDTO(Long id, String title, String subTitle, Integer year, String imgUrl, String synopsis,
 			GenreDTO genre) {
-		super();
-		this.id = id;
-		this.title = title;
-		this.subTitle = subTitle;
-		this.year = year;
-		this.imgUrl = imgUrl;
+		super(id, title, subTitle, year, imgUrl);
 		this.synopsis = synopsis;
 		this.genre = genre;
 	}
 
 	public MovieCompleteDTO(Movie entity) {
-		id = entity.getId();
-		title = entity.getTitle();
-		subTitle = entity.getSubTitle();
-		year = entity.getYear();
-		imgUrl = entity.getImgUrl();
+		super(entity);
 		synopsis = entity.getSynopsis();
 		genre = new GenreDTO(entity.getGenre());
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public String getSubTitle() {
-		return subTitle;
-	}
-
-	public void setSubTitle(String subTitle) {
-		this.subTitle = subTitle;
-	}
-
-	public Integer getYear() {
-		return year;
-	}
-
-	public void setYear(Integer year) {
-		this.year = year;
-	}
-
-	public String getImgUrl() {
-		return imgUrl;
-	}
-
-	public void setImgUrl(String imgUrl) {
-		this.imgUrl = imgUrl;
 	}
 
 	public String getSynopsis() {
